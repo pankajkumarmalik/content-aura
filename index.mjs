aac5248351da59533b29b9b7c96ed6867a6e55f1
Pankaj Kumar Malik <pankajkumarmalik6@gmail.com>
2025-03-23 11:58:45 +0530

    Clerk webhooks and Email sending via mailtrap

---
+++ index.mjs
@@ -0,0 +1,23 @@
+import { MailtrapClient } from "mailtrap";
+
+const TOKEN = process.env.MAILTRAP_API_TOKEN;
+const SENDER_EMAIL = "hello@demomailtrap.co";
+const RECIPIENT_EMAIL = "pankajkumarmalik30@gmail.com";
+
+if (!TOKEN) {
+  throw new Error("MAILTRAP_TOKEN environment variable is not set");
+}
+
+const client = new MailtrapClient({ token: TOKEN });
+
+const sender = { name: "Mailtrap Test", email: SENDER_EMAIL };
+
+client
+  .send({
+    from: sender,
+    to: [{ email: RECIPIENT_EMAIL }],
+    subject: "Hello from mailtrap",
+    text: "Welcome to ContentAura",
+  })
+  .then(console.log)
+  .catch(console.error);
